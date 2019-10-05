292ad15190fec3b4c8ed2d58fbce65c4ca92b488
i-vishi <rr.vishalgaur@gmail.com>
2019-10-05 12:56:29 +0530

    some minor additions

--- team#26-smartCar/car2.py
+++ team#26-smartCar/car2.py
@@ -1,12 +1,14 @@
 import socket
 
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+HOST = '10.42.0.1'
+PORT = 8005
 
-HOST = '127.0.0.1'
-PORT = 8000
-
-s.connect((HOST,PORT))
-message = 'hello'
-m = message.encode('utf-8')
-s.send(m)
-s.close()
+while 1:
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s.connect((HOST,PORT))
+    message = input()
+    m = message.encode('utf-8')
+    s.sendall(m)
+    v = s.recv(1024)
+    print(v.decode('utf-8'))
+    s.close()
