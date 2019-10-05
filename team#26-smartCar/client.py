b03291d5d9377b68571c1d94b141a36ec9a82ef2
i-vishi <rr.vishalgaur@gmail.com>
2019-10-05 21:02:36 +0530

    generated ip

--- team#26-smartCar/client.py
+++ team#26-smartCar/client.py
@@ -1,14 +1,27 @@
 import socket
 
-HOST = '10.42.0.1'
-PORT = 8005
+# HOST = '10.42.0.1'
+# PORT = 8005
 
-#while 1:
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect((HOST,PORT))
+s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+ip = s.getsockname()
+s.close()
+s1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ip2 = "0.1"
+ip1 = ""
+cnt = 0
+for c in range(len(ip[0])):
+    if ip[0][c] == '.':
+        cnt += 1
+    if cnt == 2:
+        ip1 = ip[0][0:c+1]
+        break
+main_ip = ip1 + ip2
+print(main_ip)
+s1.connect((main_ip, 8054))
+
 message = input()
 m = message.encode('utf-8')
-s.sendall(m)
-# v = s.recv(1024)
-# print(v.decode('utf-8'))
-s.close()
+s1.sendall(m)
+s1.close()
